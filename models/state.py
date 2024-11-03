49bf28b578df7c994e0d30bf4d7d0968e180a12e
Malik Vance <malikovance@gmail.com>
2024-11-03 16:31:00 -0600

    renaming get cities to cities

--- models/state.py
+++ models/state.py
@@ -1,25 +1,23 @@
-#!/usr/bin/python3
-"""State Module for HBNB project"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from models.engine import storage_type
+from models.__init__ import storage_type
 from sqlalchemy.orm import relationship
-
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
+    
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship('City', back_populates='state',
                               cascade="all, delete-orphan")
-
     else:
         name = ''
 
-        # getter
-        def get_cities(self):
-            from models.__init__ import storage
+        @property
+        def cities(self):
+            """Return the list of City objects from storage linked to the current State"""
+            from models import storage
             from models.city import City
             list_cities = []
             cities_dict = storage.all(City)
