415f6612a4b62006454446f0c06bdd82ecf156a0
Malik Vance <malikovance@gmail.com>
2024-11-03 16:08:35 -0600

    adjusted relationships between these files

--- models/city.py
+++ models/city.py
@@ -4,20 +4,14 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.engine import storage_type
 
-
 class City(BaseModel, Base):
     """This class defines a city by its attributes"""
     __tablename__ = 'cities'
-    if storage_type == 'db':
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-
-        # Relationship to Place
-        places = relationship("Place", back_populates="cities",
-                              cascade="all, delete-orphan")
-        # Relationship to State
-        state = relationship("State", back_populates="cities")
-
-    else:
-        state_id = ''
-        name = ''
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False) if storage_type == 'db' else ''
+    name = Column(String(128), nullable=False) if storage_type == 'db' else ''
+    
+    # Relationship to Place
+    places = relationship("Place", back_populates="cities", cascade="all, delete-orphan")
+    
+    # Relationship to State
+    state = relationship("State", back_populates="cities")
--- models/state.py
+++ models/state.py
@@ -2,28 +2,20 @@
 """State Module for HBNB project"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from models.engine import storage_type
 from sqlalchemy.orm import relationship
-
+from models.engine import storage_type
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', back_populates='state',
-                              cascade="all, delete-orphan")
+    name = Column(String(128), nullable=False) if storage_type == 'db' else ''
+    
+    # Define a relationship with City
+    cities = relationship('City', back_populates='state', cascade="all, delete-orphan")
 
-    else:
-        name = ''
-
-        # getter
-        def get_cities(self):
-            from models.__init__ import storage
-            from models.city import City
-            list_cities = []
-            cities_dict = storage.all(City)
-            for city in cities_dict.values():
-                if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+    @property
+    def get_cities(self):
+        """Getter for cities when storage type is not db"""
+        from models.__init__ import storage
+        from models.city import City
+        return [city for city in storage.all(City).values() if city.state_id == self.id]
