e829078037d9034e232858fd819d1bbdd53ea921
Malik Vance <malikovance@gmail.com>
2024-11-03 16:28:46 -0600

    added reload to both files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,17 +3,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
-from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-from models.user import User
 import os
-# Add other model imports as needed
-
 
 mysql_user = os.getenv('HBNB_MYSQL_USER')
 mysql_password = os.getenv('HBNB_MYSQL_PWD')
@@ -28,10 +24,13 @@ class DBStorage:
     __session = None
 
     classes = {
-            'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-            }
+        'User': User,
+        'Place': Place,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Review': Review
+    }
 
     def __init__(self):
         """Initialize a new DBStorage instance."""
@@ -44,24 +43,22 @@ class DBStorage:
         )
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
+        
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)
 
     def all(self, cls=None):
         """Query all objects based on class or all classes."""
         result_dict = {}
-        if cls and cls in DBStorage.classes.values():
+        if cls and cls in self.classes.values():
             query = self.__session.query(cls).all()
             for item in query:
                 result_dict['{}.{}'.format(cls.__name__, item.id)] = item
         else:
-            query = []
-            for model_class in DBStorage.classes.values():
-                query.extend(self.__session.query(model_class).all())
-            for item in query:
-                result_dict['{}.{}'.format(
-                    item.__class__.__name__, item.id)] = item
+            for model_class in self.classes.values():
+                query = self.__session.query(model_class).all()
+                for item in query:
+                    result_dict['{}.{}'.format(item.__class__.__name__, item.id)] = item
         self.__session.expire_all()
         return result_dict
 
@@ -72,9 +69,7 @@ class DBStorage:
     def save(self):
         """Commit all changes to the database."""
         try:
-            # print("DEBUG: DB STORAGE COMMITTING")  # DEBUG
             self.__session.commit()
-            self.close()
         except Exception as e:
             print("Error during commit: {}".format(e))
             self.__session.rollback()
@@ -82,18 +77,16 @@ class DBStorage:
     def reload(self):
         """Reload data from the database."""
         Base.metadata.create_all(self.__engine)
-        if self.__session:
-            self.close()
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
+        self.close()  # Ensure the previous session is closed
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)
-        # print("DEBUG: Database session reloaded")  # DEBUG
 
     def delete(self, obj=None):
+        """Delete an object from the current database session."""
         if obj:
             self.__session.delete(obj)
             self.save()
 
     def close(self):
-        """Session Closer"""
+        """Close the session."""
         self.__session.remove()
