6d2b4d267ce09070ce451107697365132da82397
Malik Vance <malikovance@gmail.com>
2024-11-03 16:41:24 -0600

    undid those files was causing more issues heading in that direction.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,18 @@
 """Defines the DBStorage engine."""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.user import User
 import os
+# Add other model imports as needed
+
 
 mysql_user = os.getenv('HBNB_MYSQL_USER')
 mysql_password = os.getenv('HBNB_MYSQL_PWD')
@@ -16,6 +27,12 @@ class DBStorage:
     __engine = None
     __session = None
 
+    classes = {
+            'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+            }
+
     def __init__(self):
         """Initialize a new DBStorage instance."""
         self.__engine = create_engine(
@@ -26,40 +43,25 @@ class DBStorage:
             pool_pre_ping=True
         )
         if os.getenv('HBNB_ENV') == 'test':
-            from models.base_model import Base
             Base.metadata.drop_all(self.__engine)
-
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
         self.__session = scoped_session(session_factory)
 
     def all(self, cls=None):
         """Query all objects based on class or all classes."""
-        from models.state import State
-        from models.city import City
-        from models.user import User
-        from models.place import Place
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            'User': User,
-            'Place': Place,
-            'State': State,
-            'City': City,
-            'Amenity': Amenity,
-            'Review': Review
-        }
-
         result_dict = {}
-        if cls and cls in classes.values():
+        if cls and cls in DBStorage.classes.values():
             query = self.__session.query(cls).all()
             for item in query:
                 result_dict['{}.{}'.format(cls.__name__, item.id)] = item
         else:
-            for model_class in classes.values():
-                query = self.__session.query(model_class).all()
-                for item in query:
-                    result_dict['{}.{}'.format(item.__class__.__name__, item.id)] = item
+            query = []
+            for model_class in DBStorage.classes.values():
+                query.extend(self.__session.query(model_class).all())
+            for item in query:
+                result_dict['{}.{}'.format(
+                    item.__class__.__name__, item.id)] = item
         self.__session.expire_all()
         return result_dict
 
@@ -70,25 +72,28 @@ class DBStorage:
     def save(self):
         """Commit all changes to the database."""
         try:
+            # print("DEBUG: DB STORAGE COMMITTING")  # DEBUG
             self.__session.commit()
+            self.close()
         except Exception as e:
             print("Error during commit: {}".format(e))
             self.__session.rollback()
 
     def reload(self):
         """Reload data from the database."""
-        from models.base_model import Base
         Base.metadata.create_all(self.__engine)
-        self.close()  # Ensure the previous session is closed
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        if self.__session:
+            self.close()
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
         self.__session = scoped_session(session_factory)
+        # print("DEBUG: Database session reloaded")  # DEBUG
 
     def delete(self, obj=None):
-        """Delete an object from the current database session."""
         if obj:
             self.__session.delete(obj)
             self.save()
 
     def close(self):
-        """Close the session."""
+        """Session Closer"""
         self.__session.remove()
