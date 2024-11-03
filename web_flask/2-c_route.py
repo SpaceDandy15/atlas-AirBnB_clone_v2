039ebd9e68e07ea5f46625818d87d29e78f5c2f2
Malik Vance <malikovance@gmail.com>
2024-11-03 14:21:59 -0600

    retyped with documentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,31 +2,50 @@
 """
 This module starts a Flask web application that responds to HTTP requests.
 
-It has three routes:
-- /: displays "Hello HBNB!".
-- /hbnb: displays "HBNB".
-- /c/<text>: displays "C " followed by the value of the text variable,
-  with underscores (_) replaced by spaces.
+The web application listens on all interfaces (0.0.0.0) at port 5000 and includes the following routes:
+
+Routes:
+- /: Displays "Hello HBNB!".
+- /hbnb: Displays "HBNB".
+- /c/<text>: Displays "C " followed by the value of the text variable,
+  replacing underscores (_) with spaces.
 """
 
-from flask import Flask
-from flask import escape
+from flask import Flask, escape
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Return a greeting message."""
+    """
+    Return a greeting message.
+
+    Route: /
+    Response: "Hello HBNB!"
+    """
     return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return the HBNB message."""
+    """
+    Return the HBNB message.
+
+    Route: /hbnb
+    Response: "HBNB"
+    """
     return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-    """Return 'C ' followed by the value of the text variable."""
+    """
+    Return 'C ' followed by the value of the text variable.
+
+    Route: /c/<text>
+    Parameter:
+    - text (str): The text to display after "C ".
+    
+    Response: "C <text>", where underscores in <text> are replaced by spaces.
+    """
     return "C {}".format(escape(text.replace('_', ' ')))
 
 if __name__ == "__main__":
