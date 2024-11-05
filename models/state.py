5c80871fcf5a41a449e18ae29fbef1c0a956a37d
Malik Vance <malikovance@gmail.com>
2024-11-05 11:52:39 -0600

    properly added methods for each file

--- models/state.py
+++ models/state.py
@@ -9,20 +9,21 @@ from models.engine import storage_type
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
+    
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', back_populates='state',
-                              cascade="all, delete-orphan")
+        cities = relationship('City', back_populates='state', cascade="all, delete-orphan")
     else:
         name = ''
 
-        def get_cities(self):
-            """Getter for cities if using file storage."""
-            from models.__init__ import storage
-            from models.city import City
-            list_cities = []
-            cities_dict = storage.all(City)
-            for city in cities_dict.values():
-                if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+    def cities(self):
+        """Getter for cities if using file storage."""
+        from models.__init__ import storage
+        from models.city import City
+        
+        list_cities = []
+        cities_dict = storage.all(City)
+        for city in cities_dict.values():
+            if city.state_id == self.id:
+                list_cities.append(city)
+        return list_cities
